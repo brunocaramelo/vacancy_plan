e776a8b9305d5e9714510a4049fbf7ede1d2b5fb
Bruno Caramelo <bruno.caramelo5@gmail.com>
2024-08-06 03:38:50 -0300

    rewrites

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,7 +7,7 @@ namespace App\Http\Controllers;
  *   version="1.0.0",
  *   title="API Holiday Plans",
  *      title="API Holiday Plans - API",
- *       description="based OpenApi",
+ *       description="First step, go to the TAG login, execute the POST /api/login, then retrieve the value of the response: access_token, then at the top press the green Authorize button and paste the value into the field: bearerAuth (http, Bearer) of the modal, and press the green Authorize button to access the TAGS: holidays and report.",
  *       @OA\Contact(
  *           email="bruno.caramelo5@gmail.com"
  *       ),
